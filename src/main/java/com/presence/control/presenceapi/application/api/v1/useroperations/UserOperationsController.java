df1ca129cafcce4fd02eabd315b698769697fba9
Augusto Santos <augusto.santos@AC-augustosantos.local>
2020-12-11 20:16:57 -0300

    Adding Api response codes to swagger documentation

--- src/main/java/com/presence/control/presenceapi/application/api/v1/useroperations/UserOperationsController.java
+++ src/main/java/com/presence/control/presenceapi/application/api/v1/useroperations/UserOperationsController.java
@@ -4,6 +4,8 @@ import com.presence.control.presenceapi.application.response.Response;
 import com.presence.control.presenceapi.data.dto.UserDTO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,11 @@ import javax.validation.Valid;
 
 @RequestMapping(path = "/api/useroperations/v1/")
 @Api(value = "Presence Control User Operations API")
+@ApiResponses(value = {
+        @ApiResponse(code = 201, message = "User created"),
+        @ApiResponse(code = 409, message = "User already exists with specified email"),
+        @ApiResponse(code = 400, message = "Wrong parameters in the request message")
+})
 public interface UserOperationsController {
 
     @ApiOperation("Create a new User")
